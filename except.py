4f9d58e489f6779d6e824ef42039b91a71f94fc6
revillnik <revillnik@gmail.com>
2024-01-06 19:38:18 +0300

    next

--- except.py
+++ except.py
@@ -2,11 +2,13 @@ try:
     z = [i for i in input().split(" ")]
     z = list(map(int, z))
     f = int(input())
-    res = sum(z) / f
+    try:
+        res = sum(z) / f
+    except ZeroDivisionError as z:
+        print(z)
 except ValueError:
     print("Допустимо вводить только числа через пробел")
-except ZeroDivisionError:
-    print('В качестве знаменателя не может быть ноль')
-except:
-    print('Неизвестная ошибка')
-print('Штатное завершение программы')
+else:
+    print("Штатное выполнение программы")
+finally:
+    print("Анализ закончен")
