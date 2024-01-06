c93498fa32a8b46c1adbb6bd87ffd92636030a79
revillnik <revillnik@gmail.com>
2024-01-06 19:40:15 +0300

    next

--- except.py
+++ except.py
@@ -1,11 +1,14 @@
+def check_zero(x, y):
+    try:
+        res = x / y
+        return res
+    except ZeroDivisionError as z:
+        print(z)
 try:
     z = [i for i in input().split(" ")]
     z = list(map(int, z))
     f = int(input())
-    try:
-        res = sum(z) / f
-    except ZeroDivisionError as z:
-        print(z)
+    check_zero(sum(z), f)
 except ValueError:
     print("Допустимо вводить только числа через пробел")
 else:
